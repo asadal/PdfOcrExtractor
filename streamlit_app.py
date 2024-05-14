b56c9e2d3dae53ee6df13ea3913804bb2f1753ad
asadal <blaulaut@gmail.com>
2024-05-15 02:18:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ def get_tmp_filepath(uploaded_file):
         tmp_filepath = tmp_file.name
         print("temporary_filepath : ", tmp_filepath)
         return tmp_filepath
+
 def extract_text_from_img(img_path):
     text_string = pytesseract.image_to_string(img_path, lang='kor+eng').strip()
     print(text_string)
@@ -32,39 +33,21 @@ def output_text(st, final_txt):
     )
 
 def app():
-    # Set page title and icon
-    st.set_page_config(
-        page_title="PDF Extractor",
-        page_icon="https://cdn-icons-png.flaticon.com/512/29/29099.png"
-    )
-
-    # Featured image
-    st.image(
-        "https://cdn-icons-png.flaticon.com/512/29/29099.png",
-        width=120
-    )
-
-    # Main title and description
+    st.set_page_config(page_title="PDF Extractor", page_icon="https://cdn-icons-png.flaticon.com/512/29/29099.png")
+    st.image("https://cdn-icons-png.flaticon.com/512/29/29099.png", width=120)
     st.title("텍스트 추출기")
     st.subheader("이미지 파일에서 텍스트를 추출해줍니다.")
     st.subheader("광학문자인식(OCR) 기능을 이용합니다.")
     st.write("이미지(jpg, png...) 파일을 묶은 PDF에서 이미지 파일을 분리한 다음, 각 이미지에 포함된 글자들을 OCR로 추출합니다.")
     st.markdown("PDF 문서 파일에서 텍스트만 분리하시려면 [**PDF Extractor**](https://asadal-pdf.streamlit.app/)를 이용해주세요.")
-    
-    if 'key' not in st.session_state:
-        st.session_state['key'] = 'value'
-    
+
     pdf_file = st.file_uploader("PDF 파일을 업로드하세요.", type="pdf", key='pdf_file')
-    
     st.divider()
-    
     img_file = st.file_uploader("이미지 파일을 업로드하세요.")
-    
     st.divider()
-    
+
     if pdf_file is not None:
         try:
-#             os.system("brew install tesseract tesseract-lang && brew link tesseract")
             with st.spinner("텍스트를 추출하고 있습니다..."):
                 with NamedTemporaryFile(delete=True) as tmp_file:
                     tmp_file.write(pdf_file.getvalue())
@@ -78,16 +61,17 @@ def app():
                         img = cv2.imread(pdf_filepath + f"page_{i+1}.jpg")
                         text_string = extract_text_from_img(img)
                         final_txt += text_string
+            output_text(st, final_txt)
         except Exception as e:
             st.error("오류가 발생했습니다. 😥")
             st.error(e)
-        
-        output_text(st, final_txt)
-        pdf_file.__del__()
-        
+        finally:
+            if 'pdf_file' in st.session_state:
+                del st.session_state['pdf_file']
+            pdf_file.__del__()
+
     elif img_file is not None:
         try:
-#             os.system("brew install tesseract tesseract-lang && brew link tesseract")
             with st.spinner("텍스트를 추출하고 있습니다..."):
                 with NamedTemporaryFile(delete=True) as tmp_file:
                     tmp_file.write(img_file.getvalue())
@@ -96,16 +80,18 @@ def app():
                     final_txt = ""
                     text_string = extract_text_from_img(img_filepath)
                     final_txt += text_string
+            output_text(st, final_txt)
         except Exception as e:
             st.error("오류가 발생했습니다. 😥")
             st.error(e)
-        
-        output_text(st, final_txt)
-        img_file.__del__()
-        
+        finally:
+            if 'img_file' in st.session_state:
+                del st.session_state['img_file']
+            img_file.__del__()
+
     else:
         st.write("파일을 업로드하거나 끌어다 놓으세요.")
         st.stop()
-        
+
 if __name__ == "__main__":
     app()
