54395cf41273ebc23178dff154903376f2032689
asadal <blaulaut@gmail.com>
2024-05-15 09:15:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,22 +8,13 @@ from tqdm import tqdm
 from pdf2image import convert_from_path
 import cv2
 
-def get_tmp_filepath(uploaded_file):
-    with NamedTemporaryFile(delete=True) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_filepath = tmp_file.name
-        print("temporary_filepath : ", tmp_filepath)
-        return tmp_filepath
-
-def extract_text_from_img(img_path):
-    text_string = pytesseract.image_to_string(img_path, lang='kor+eng').strip()
-    print(text_string)
+def extract_text_from_img(img):
+    text_string = pytesseract.image_to_string(img, lang='kor+eng').strip()
     return text_string
 
 def output_text(st, final_txt):
     st.header("추출된 텍스트")
     st.text_area("", value=final_txt, height=500)
-    # 추출된 텍스트를 txt 파일로 다운로드
     st.header("텍스트 다운로드")
     st.download_button(
         label="📥 Download Timeline Script ⏱",
@@ -35,63 +26,46 @@ def output_text(st, final_txt):
 def app():
     st.set_page_config(page_title="PDF Extractor", page_icon="https://cdn-icons-png.flaticon.com/512/29/29099.png")
     st.image("https://cdn-icons-png.flaticon.com/512/29/29099.png", width=120)
-    st.title("텍스트 추출기")
+    col1, col2 = st.columns([8, 2])
+    with col1:
+        st.title("텍스트 추출기")
+    with col2:
+        if st.button("clear ↺"):
+            st.session_state.clear()  # 상태 초기화
+            st.experimental_rerun()  # 재실행
+    
     st.subheader("이미지 파일에서 텍스트를 추출해줍니다.")
     st.subheader("광학문자인식(OCR) 기능을 이용합니다.")
     st.write("이미지(jpg, png...) 파일을 묶은 PDF에서 이미지 파일을 분리한 다음, 각 이미지에 포함된 글자들을 OCR로 추출합니다.")
     st.markdown("PDF 문서 파일에서 텍스트만 분리하시려면 [**PDF Extractor**](https://asadal-pdf.streamlit.app/)를 이용해주세요.")
 
-    pdf_file = st.file_uploader("PDF 파일을 업로드하세요.", type="pdf", key='pdf_file')
-    st.divider()
+    pdf_file = st.file_uploader("PDF 파일을 업로드하세요.", type="pdf")
     img_file = st.file_uploader("이미지 파일을 업로드하세요.")
-    st.divider()
 
-    if pdf_file is not None:
-        try:
+    if pdf_file:
+        if 'final_txt' not in st.session_state:
             with st.spinner("텍스트를 추출하고 있습니다..."):
-                with NamedTemporaryFile(delete=True) as tmp_file:
+                with NamedTemporaryFile(delete=False) as tmp_file:
                     tmp_file.write(pdf_file.getvalue())
                     pdf_filepath = tmp_file.name
-                    print("temporary_filepath : ", pdf_filepath)
-                    final_txt = ""
                     images = convert_from_path(pdf_filepath)
-                    print("전체 페이지 수 : ", len(images))
-                    for i, image in enumerate(images):
-                        image.save(pdf_filepath + f"page_{i+1}.jpg", "JPEG")
-                        img = cv2.imread(pdf_filepath + f"page_{i+1}.jpg")
-                        text_string = extract_text_from_img(img)
+                    final_txt = ''
+                    for image in images:
+                        text_string = extract_text_from_img(image)  # 이미지 객체 직접 전달
                         final_txt += text_string
-            output_text(st, final_txt)
-        except Exception as e:
-            st.error("오류가 발생했습니다. 😥")
-            st.error(e)
-        finally:
-            if 'pdf_file' in st.session_state:
-                del st.session_state['pdf_file']
-            pdf_file.__del__()
 
-    elif img_file is not None:
-        try:
+                    st.session_state['final_txt'] = final_txt
+        output_text(st, st.session_state['final_txt'])
+
+    elif img_file:
+        if 'final_txt' not in st.session_state:
             with st.spinner("텍스트를 추출하고 있습니다..."):
-                with NamedTemporaryFile(delete=True) as tmp_file:
+                with NamedTemporaryFile(delete=False) as tmp_file:
                     tmp_file.write(img_file.getvalue())
                     img_filepath = tmp_file.name
-                    print("temporary_filepath : ", img_filepath)
-                    final_txt = ""
-                    text_string = extract_text_from_img(img_filepath)
-                    final_txt += text_string
-            output_text(st, final_txt)
-        except Exception as e:
-            st.error("오류가 발생했습니다. 😥")
-            st.error(e)
-        finally:
-            if 'img_file' in st.session_state:
-                del st.session_state['img_file']
-            img_file.__del__()
-
-    else:
-        st.write("파일을 업로드하거나 끌어다 놓으세요.")
-        st.stop()
+                    final_txt = extract_text_from_img(img_filepath)
+                    st.session_state['final_txt'] = final_txt
+        output_text(st, st.session_state['final_txt'])
 
 if __name__ == "__main__":
     app()
