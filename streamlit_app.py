8467a9f0acdd3fd64bedfb717485b2f442aad3df
asadal <blaulaut@gmail.com>
2024-07-23 10:24:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import re
 
 def extract_text_from_img(img):
     # pytesseract.pytesseract.tesseract_cmd = '/tesseract-ocr/tessdata/'
-    config = '-l kor+eng, --tessdata-dir /tessdata/'.format(lang, PATH)
+    config = ('-l kor+eng, --tessdata-dir /tessdata/')
     text_string = pytesseract.image_to_string(img, config=config).strip()
     return text_string
 
